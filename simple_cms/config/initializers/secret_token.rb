18e7470b489d0ab440e2268c33aa196f24f6cd8c
kamal patwa <kamalptw@gmail.com>
2015-09-11 11:07:41 +0530

    Add simple_cms rails project

---
+++ simple_cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '5bd863e293550b63239f098d1310ae70f0c3e6763c09c8de0371ea72c36c4212def511a892a11b6eae4975c50f699042b79e034a5afcac6440371fb3cbe77668'
