4c48b8c2d51bef7cf5e13999517bb1d52814e6a5
kamal patwa <kamalptw@gmail.com>
2015-09-14 00:35:11 +0530

    Add all files up to date with lesson 6

---
+++ simple_cms/db/migrate/20150913135147_do_nothing_yet.rb
@@ -0,0 +1,9 @@
+class DoNothingYet < ActiveRecord::Migration
+  def up
+  	#instruction for setting db to a new state
+  end
+
+  def down
+  	#instruction for setting db back to the previous state
+  end
+end
